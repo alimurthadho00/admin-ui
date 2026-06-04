965a72d1ee3d3f33638476606dc0a41b880c6b79
alimurthadho00 <hpredmia5@gmail.com>
2026-06-04 12:11:51 +0700

    14-State-Management

--- src/components/Elements/CompositionExample.jsx
+++ src/components/Elements/CompositionExample.jsx
@@ -4,11 +4,17 @@ import {
   GaugeReferenceArc,
   useGaugeState,
 } from "@mui/x-charts/Gauge";
+import * as React from "react"
+import { ThemeContext } from "../../context/themeContext";
+import { data } from "react-router-dom";
 
-function GaugePointer() {
+function GaugePointer(props) {
+ const { color } = props;
+
   const { valueAngle, outerRadius, cx, cy } = useGaugeState();
 
   if (valueAngle === null) {
+    // No value to display
     return null;
   }
 
@@ -19,10 +25,10 @@ function GaugePointer() {
 
   return (
     <g>
-      <circle cx={cx} cy={cy} r={5} fill="#299D91" />
+      <circle cx={cx} cy={cy} r={5} fill={color} />
       <path
         d={`M ${cx} ${cy} L ${target.x} ${target.y}`}
-        stroke="#299D91"
+        stroke={color}
         strokeWidth={3}
       />
     </g>
@@ -31,6 +37,7 @@ function GaugePointer() {
 
 export default function CompositionExample(props) {
   const { value } = props;
+  const { theme } = React.useContext(ThemeContext);
 
   return (
     <GaugeContainer
@@ -39,15 +46,10 @@ export default function CompositionExample(props) {
       startAngle={-90}
       endAngle={90}
       value={value}
-      sx={() => ({
-        [`& .value-arc`]: {
-          fill: "#299D91",
-        },
-      })}
     >
       <GaugeReferenceArc />
-      <GaugeValueArc className="value-arc" />
-      <GaugePointer />
+      <GaugeValueArc sx={{ fill: theme.color }} />
+      <GaugePointer color={theme.color} />
     </GaugeContainer>
   );
 }
--- src/components/Elements/Logo.jsx
+++ src/components/Elements/Logo.jsx
@@ -5,7 +5,7 @@ function Logo(props) {
 
   const variantClasses = {
     primary: "text-primary text-4xl",
-    secondary: "text-white text-sm sm:text-2xl",
+    secondary: "text-primary text-sm sm:text-2xl",
   };
 
   return (
@@ -22,4 +22,4 @@ function Logo(props) {
   );
 }
 
-export default Logo;
+export default Logo;  
