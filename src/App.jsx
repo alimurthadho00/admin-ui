b78a28a8d57d518c1bb6d9b8c9eda7c0df0eb077
alimurthadho00 <hpredmia5@gmail.com>
2026-06-09 07:14:55 +0700

    perbaiki vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
 import ErrorPage from "./pages/error";
-import DashboardPage from "./pages/dashboard";
+import DashboardPage from "./pages/Dashboard";
 import BalancePage from "./pages/Balance";
 import {createBrowserRouter,RouterProvider,Link,} from "react-router-dom";
 function App() {
